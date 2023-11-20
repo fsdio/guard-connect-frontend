8ba9e395d3b4da8d6207255d2cb66a8552608aea
Rahmat Sunjani <klikrahmatsunjani@gmail.com>
2023-11-20 15:02:52 +0700

    Revision Config

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-let BASE_URL = "https://post-it-heroku.herokuapp.com/";
+let BASE_URL = "https://guard-connect-frontend.vercel.app/";
 if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
   BASE_URL = "http://localhost:4000/";
 }
